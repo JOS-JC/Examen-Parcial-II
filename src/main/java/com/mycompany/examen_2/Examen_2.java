5adadec345c59d4c77a279393b1ce6f5c40a515f
JOS-JC <josealfredojulajujchumil@gmail.com>
2024-10-11 23:28:30 -0600

    first commit

---
+++ src/main/java/com/mycompany/examen_2/Examen_2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.examen_2;
+
+/**
+ *
+ * @author josea
+ */
+public class Examen_2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
